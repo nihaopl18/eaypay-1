e8c583ac73a9fcb09a9e54c177b82fa68e47133c
tlhhup <137273278@qq.com>
2017-01-10 11:05:29 +0800

    i

--- WebContent/scripts/function-manage.js
+++ WebContent/scripts/function-manage.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 // JavaScript Document
 function Delete(id)
 {
 	if(confirm("确定要删除吗？")) {
 		location.href = "user-delete.html?id=" + id;
 	}
+=======
+// JavaScript Document
+function Delete(id)
+{
+	if(confirm("确定要删除吗？")) {
+		location.href = "user-delete.html?id=" + id;
+	}
+>>>>>>> 27bc9fda51af523730bd47a791c3c2471028c5ea
 }
