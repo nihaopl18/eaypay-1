ec70816af2b173e8a35332c72d0e45f72d2f5643
tlhhup <137273278@qq.com>
2017-01-10 11:00:40 +0800

    1.解决user的冲突

--- src/com/woniuxy/eaypay/entity/User.java
+++ src/com/woniuxy/eaypay/entity/User.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.woniuxy.eaypay.entity;
 
 public class User {
 
 }
-=======
-package com.woniuxy.eaypay.entity;
-
-public class User {
-
-}
->>>>>>> 27bc9fda51af523730bd47a791c3c2471028c5ea
